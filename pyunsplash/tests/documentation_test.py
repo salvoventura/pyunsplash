63d29126a2a6ea9ce85af0a190ce51545ab49b7b
salvo <salvoventura@gmail.com>
2017-09-06 17:04:27 -0700

    - Completed

--- pyunsplash/tests/documentation_test.py
+++ pyunsplash/tests/documentation_test.py
@@ -1,6 +1,7 @@
 import pyunsplash
 import logging
-API_KEY='ea8d39d41467c530ce8aa50b36cc1dd31d8527e6608656b729c270b9b8eaae79'
+
+API_KEY = 'ea8d39d41467c530ce8aa50b36cc1dd31d8527e6608656b729c270b9b8eaae79'
 
 
 def funzione_1():
@@ -42,8 +43,6 @@ def funzione_6(pu, logger):
     logger.info('Funzione_6')
     pu.init_logging(logging.DEBUG)
     # use the PyUnsplash objects: all logs will be recorded to log file
-
-
 
 
 # API: Class Collection
@@ -116,8 +115,6 @@ def funzione_15(pu, logger):
             print rel_collection.title, rel_collection.description
 
 
-
-
 # API: Class Collections
 
 def funzione_16(pu, logger):
@@ -126,8 +123,6 @@ def funzione_16(pu, logger):
     collections = this_user.collections(page=1, per_page=5)
     for collection in collections.entries:
         print collection.id, collection.title
-
-
 
 
 # API: Class Photo
@@ -144,7 +139,7 @@ def funzione_17(pu, logger):
 def funzione_18(pu, logger):
     logger.info('Funzione_18')
     this_user = pu.user('salvoventura', w=100, h=100)
-    photos = this_user.photos()    # photos is an instance of class Photos
+    photos = this_user.photos()  # photos is an instance of class Photos
     for photo in photos.entries:
         photo.refresh()
         print photo.id, photo.link_download
@@ -174,8 +169,6 @@ def funzione_21(pu, logger):
         print photo.stats
 
 
-
-
 # API: Class Photos
 
 def funzione_22(pu, logger):
@@ -184,8 +177,6 @@ def funzione_22(pu, logger):
     photos = this_user.photos()  # photos is an instance of class Photos
     for photo in photos.entries:
         print photo.id, photo.link_download
-
-
 
 
 # API: Class Search
@@ -197,15 +188,12 @@ def funzione_23(pu, logger):
         print photo.id, photo.link_download
 
 
-
 # API: Class Stats
 
 def funzione_24(pu, logger):
     logger.info('Funzione_24')
     stats = pu.stats()
     print stats.total
-
-
 
 
 # API: Class User
@@ -215,40 +203,48 @@ def funzione_25(pu, logger):
     this_user = pu.user('salvoventura', w=100, h=100)
     this_user.refresh()
 
+
 def funzione_26(pu, logger):
     logger.info('Funzione_26')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.id
+
 
 def funzione_27(pu, logger):
     logger.info('Funzione_27')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.links
 
+
 def funzione_28(pu, logger):
     logger.info('Funzione_28')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.link_html
+
 
 def funzione_29(pu, logger):
     logger.info('Funzione_29')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.link_portfolio
 
+
 def funzione_30(pu, logger):
     logger.info('Funzione_30')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.link_followers
+
 
 def funzione_31(pu, logger):
     logger.info('Funzione_31')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.link_following
 
+
 def funzione_32(pu, logger):
     logger.info('Funzione_32')
     this_user = pu.user('salvoventura', w=100, h=100)
     print this_user.link_photos
+
 
 def funzione_33(pu, logger):
     logger.info('Funzione_33')
@@ -257,12 +253,14 @@ def funzione_33(pu, logger):
     for photo in photos.entries:
         print photo.id, photo.link_download
 
+
 def funzione_34(pu, logger):
     logger.info('Funzione_34')
     this_user = pu.user('salvoventura', w=100, h=100)
     followers = this_user.followers()
     for user in followers.entries:
         print user.id, user.body.get('first_name'), user.body.get('last_name')
+
 
 def funzione_35(pu, logger):
     logger.info('Funzione_35')
@@ -271,6 +269,7 @@ def funzione_35(pu, logger):
     for user in following.entries:
         print user.id, user.body.get('first_name'), user.body.get('last_name')
 
+
 def funzione_36(pu, logger):
     logger.info('Funzione_36')
     this_user = pu.user('salvoventura', w=100, h=100)
@@ -278,14 +277,13 @@ def funzione_36(pu, logger):
     for photo in photos.entries:
         print photo.id, photo.link_download
 
+
 def funzione_37(pu, logger):
     logger.info('Funzione_37')
     this_user = pu.user('salvoventura', w=100, h=100)
     collections = this_user.collections(page=1, per_page=5)
     for collection in collections.entries:
         print collection.id, collection.title
-
-
 
 
 # API: Class Users
@@ -296,30 +294,6 @@ def funzione_38(pu, logger):
     followers = this_user.followers()  # followers is an instance of class Users
     for user in followers.entries:
         print user.id, user.body.get('first_name'), user.body.get('last_name')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 def main():
@@ -360,8 +334,7 @@ def main():
     funzione_35(pu, logger)
     funzione_36(pu, logger)
     funzione_37(pu, logger)
-
-
+    funzione_38(pu, logger)
 
 
 if __name__ == '__main__':
