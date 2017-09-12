2479fc91f86e44b999f36b3315f8cf4ed613c66d
salvo <salvoventura@gmail.com>
2017-09-11 22:50:47 -0700

    - Metadata additions

--- pyunsplash/__init__.py
+++ pyunsplash/__init__.py
@@ -9,9 +9,14 @@
 #   Comment:
 #
 ###############################################################################
+
+"""An open source Python wrapper for the Unsplash REST API"""
+
 from pyunsplash import PyUnsplash
 # TRAVIS_BUILD_NUMBER
 # TRAVIS_COMMIT
 # TRAVIS_COMMIT_RANGE
 # TRAVIS_TAG
 __version__ = '1.0.0b3'
+__author__ = 'Salvatore Ventura <salvoventura@gmail.com>'
+__license__ = 'MIT'
