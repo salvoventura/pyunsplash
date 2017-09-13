c4751956c3412829c7de1abef9747a4f23c8c80a
salvo <salvoventura@gmail.com>
2017-09-12 21:18:02 -0700

    - Remove deploy section from Travis

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='pyunsplash',
-    version='1.0.0b3',  # beta, v3
+    version='1.0.0b4',  # beta, v4
     packages=['pyunsplash', 'pyunsplash.src'],
     url='https://github.com/salvoventura/pyunsplash',
     license='MIT',
