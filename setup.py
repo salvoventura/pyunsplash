8a0b99828efa6636215837d49dee0d6c62cc5752
salvoventura <venturasalvatore@yahoo.it>
2018-11-29 18:43:35 -0800

    - Add support for Python3: fix setup.py file

--- setup.py
+++ setup.py
@@ -25,11 +25,12 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
     keywords=['unsplash', 'rest', 'api', 'python', 'wrapper', 'development', 'unsplash.com', 'photography'],
     install_requires=['requests'],
-    python_requires='>=2.7, <3'
+    python_requires='>=2.7'
 )
 
