84a22a4442dce6360c4a4938b0653308dfba3cc3
Ömer Boratav <oboratav@sfu.ca>
2022-10-25 16:09:16 -0700

    Update valid options

--- pyunsplash/src/search.py
+++ pyunsplash/src/search.py
@@ -20,20 +20,43 @@ from .users import User
 
 class Search(UnsplashPage):
     def __init__(self, api_key, url, **kwargs):
-        valid_options = ['page', 'per_page', 'query']
-        self.url = url
-        if url.lower() in ['photos', 'collections', 'users']:
-            self.url = url.lower()
-            url = '/search/{}'.format(url.lower())
-        super(Search, self).__init__(url=url, api_key=api_key, valid_options=valid_options, **kwargs)
+        valid_options = {
+                "photos": [
+                    "query",
+                    "page",
+                    "per_page",
+                    "order_by",
+                    "collections",
+                    "content_filter",
+                    "color",
+                    "orientation",
+                    "lang", # Beta parameter (for access to beta parameters, email api@unsplash.com with your application ID)
+                ],
+                "collections": [
+                    "query",
+                    "page",
+                    "per_page",
+                ],
+                "users": [
+                    "query",
+                    "page",
+                    "per_page",
+                ],
+        }
+        self.url = url.lower()
+        if self.url in valid_options:
+            url = "/search/{}".format(url)
+        super(Search, self).__init__(
+            url=url, api_key=api_key, valid_options=valid_options[self.url], **kwargs
+        )
 
     @property
     def entries(self):
-        if self.url == 'photos' or self.url.endswith('/photos'):
+        if self.url == "photos" or self.url.endswith("/photos"):
             class_cast = Photo
-        elif self.url == 'collections' or self.url.endswith('/collections'):
+        elif self.url == "collections" or self.url.endswith("/collections"):
             class_cast = Collection
-        elif self.url == 'users' or self.url.endswith('/users'):
+        elif self.url == "users" or self.url.endswith("/users"):
             class_cast = User
-        for entry in self.body.get('results', []):
+        for entry in self.body.get("results", []):
             yield class_cast(api_key=self.api_key, source=entry)
